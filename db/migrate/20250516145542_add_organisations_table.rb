340b599c6c856fe4c6eaa8679ebf9c97278e8256
Rob van Dijk <697696+robvandijk@users.noreply.github.com>
2025-05-17 09:27:05 +0200

    Adds creating Discourse users

--- db/migrate/20250516145542_add_organisations_table.rb
+++ db/migrate/20250516145542_add_organisations_table.rb
@@ -2,7 +2,7 @@
 class AddOrganisationsTable < ActiveRecord::Migration[7.2]
   def change
     create_table :organisations do |t|
-      t.integer :system_identifier
+      t.integer :system_identifier, index: { unique: true, name: "unique_organisations" }
       t.string :name
       t.string :woo_email
       t.timestamps
--- plugin.rb
+++ plugin.rb
@@ -3,37 +3,64 @@
 # version: 0.0.1
 # authors: Open State
 # url: https://github.com/yourusername/basic-plugin
+require 'sidekiq/api'
+
 class ::Organisation < ActiveRecord::Base
 
 end
 
-def get_organisaties
-    print "RVD in get_organisaties!"
-    ::Organisation.delete_all()
+def create_user(name, email)
+    username = name.parameterize[0, 60]
+    if username.end_with?('-')
+        username = username[0, 59]
+    end
+
+    new_user_params = {
+        name: name,
+        email: email,
+        username: username,
+        username_lower: username.downcase,
+        active: true,
+        approved: true
+    }
+    user ||= User.new
+    user.attributes = new_user_params
+    ReviewableUser.set_approved_fields!(user, Discourse.system_user)
+
+    user.save!
+end
+
+def get_organisations
+    print "Start of get_organisations!"
+
     doc = Nokogiri::XML(File.open("plugins/terminal-zoo-get-organisations/exportOO.xml"))
     organisations = []
+
     doc.xpath("//p:organisatie").each do |organisation|
         system_identifier = organisation['p:systeemId']
         name = organisation.xpath("./p:naam/text()")[0].to_s
         woo_email = organisation.xpath(".//p:naam[text()='Woo-contactpersoon']/..//p:email/text()")[0].to_s
-        if woo_email.blank?
+
+        if EmailAddressValidator.valid_value?(woo_email)
+            if !User.find_by_email(woo_email)
+                create_user(name, woo_email)
+            end
+        else
             woo_email = nil
-        # else
-        #     UsersController
         end
-        print("org: #{system_identifier} #{name} #{woo_email}")
+
         db_org = {system_identifier: system_identifier, name: name, woo_email: woo_email}
         organisations << db_org
     end
+
     if organisations.length > 0
-        ::Organisation.insert_all organisations
+        ::Organisation.upsert_all(organisations, unique_by: :system_identifier)
     end
 
-    print "RVD end of get_organisaties!"
+    print "End of get_organisations!"
 end
 
 
 after_initialize do
-  # Code which should run after Rails has finished booting
-    get_organisaties()
+    get_organisations()
 end
